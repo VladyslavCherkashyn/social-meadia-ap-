eff82b718b26221b006f97774069301a5941b159
VladyslavCherkashyn <Sparrowq2@gmail.com>
2023-05-05 11:52:51 +0300

    Ñremoved unrealized models

--- blog/models.py
+++ blog/models.py
@@ -100,15 +100,3 @@ class Post(models.Model):
     content = models.TextField()
     media = models.ImageField(upload_to="posts/", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
-
-class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
-
-class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
